a81a63231c14788c8c8cff6fd2df25d96f3c5de1
laffed <roarklaffed@gmail.com>
2021-05-07 19:11:29 -0500

    added ant design

--- web/src/state/index.ts
+++ web/src/state/index.ts
@@ -1,1 +1,23 @@
-export { }
+import {IConfig, IOnInitialize, IAction, IOperator, IState} from 'overmind';
+import {createHook} from 'overmind-react';
+import {namespaced} from 'overmind/config';
+import * as user from './user';
+
+export const config = namespaced({
+  user,
+});
+
+export interface Config extends IConfig<{
+  state: typeof config.state,
+  actions: typeof config.actions
+}> { }
+
+export interface OnInitialize extends IOnInitialize<Config> { }
+
+export interface Action<Input = void, Output = void> extends IAction<Config, Input, Output> { }
+
+export interface AsyncAction<Input = void, Output = void> extends IAction<Config, Input, Promise<Output>> { }
+
+export interface Operator<Input = void, Output = Input> extends IOperator<Config, Input, Output> { }
+
+export const useOvermind = createHook<typeof config>();
